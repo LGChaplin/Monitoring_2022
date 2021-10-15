0bbd7e220918c367f203b129c86727a9b6bbe114
LGChaplin <75246273+LGChaplin@users.noreply.github.com>
2021-10-15 11:04:23 +0200

    Create R-first_code.r

---
+++ R-first_code.r
@@ -0,0 +1,13 @@
+# This part with the hashtag is a part that R can't read but is your own space for commenting the code to understand what it is doing
+
+# This is the first code in github
+
+# Here are the input data
+# Costanza data on streams 
+water ‹- c(100, 200, 300, 400, 500)
+water
+
+fishes ‹- c(10, 50, 60, 80, 100)
+fishes
+
+plot(water, fishes)
