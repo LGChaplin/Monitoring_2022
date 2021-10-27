95615129e00b42ce59f1151fe050aa21041b72b8
LGChaplin <75246273+LGChaplin@users.noreply.github.com>
2021-10-28 00:28:21 +0200

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,13 @@
+# R code for EM 
+# Need to install other packages which aren't already in R
+# Search for raster package to manage image data and install it
+# https://cran.r-project.org/web/packages/raster/index.html
+
+install.packages("raster")
+# Need the quote marks to install packages outside of R
+
+# There are functions to use installed packages 
+# Packages can be found in libraries 
+
+library(raster)
+# Don't need quote marks bc the package has been installed and therefore is in R
